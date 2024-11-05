839f8eaf34c8f9a74fb8a445f3a40ddce2169627
Anay Sehgal <anaysehgal1@gmail.com>
2024-11-05 17:29:56 +0530

    Add firebase config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { firebaseConfig } from './utils/firebase'
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import { getAuth } from 'firebase/auth'
+import { appIJK, authIJK, dbIJK } from './utils/injects'
+import { provide } from 'vue'
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getDatabase(app)
+
+provide(appIJK, app)
+provide(authIJK, auth)
+provide(dbIJK, db)
 </script>
 
 <template>
